ffe3b0fb5257b053412e781abd98814b00af740e
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-11 18:09:05 +0200

    img add and nav add

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
-let div = ""
-let page = 1
-let transitionin = 0
-let transitionout = 0
+// let div = ""
+// let page = 1
+// let transitionin = 0
+// let transitionout = 0
 
-function pageactuelle(page) {
-    if (page == 1){
-        transitionin = 100
-        transitionout = 0
-    }
-    if (page == 2){
-        transitionin = 200
-        transitionout = 0
-    }
-    if (page == 3){
-        transitionin = 300
-        transitionout = -100
-    }
-    if (page == 4){
-        transitionin = 400
-        transitionout = -200
-    }
-}
+// function pageactuelle(page) {
+//     if (page == 1){
+//         transitionin = 100
+//         transitionout = 0
+//     }
+//     if (page == 2){
+//         transitionin = 200
+//         transitionout = 0
+//     }
+//     if (page == 3){
+//         transitionin = 300
+//         transitionout = -100
+//     }
+//     if (page == 4){
+//         transitionin = 400
+//         transitionout = -200
+//     }
+// }
 
-addEventListener('wheel', (event)=> {
-    pageactuelle(page)
-    if (event.deltaY > 0) {
-        if (page < 4){
-            div = document.getElementById("page")
-            div.setAttribute("style", `translate : ${-transitionin}vw`)
-            page += 1 
-        }
-    }else if(event.deltaY < 0){
-            div.setAttribute("style", `translate : ${transitionout}vw`) 
-            page -=1
-    } 
-})
+// addEventListener('wheel', (event)=> {
+//     pageactuelle(page)
+//     if (event.deltaY > 0) {
+//         if (page < 4){
+//             div = document.getElementById("page")
+//             div.setAttribute("style", `translate : ${-transitionin}vw`)
+//             page += 1 
+//         }
+//     }else if(event.deltaY < 0){
+//             div.setAttribute("style", `translate : ${transitionout}vw`) 
+//             page -=1
+//     } 
+// })
