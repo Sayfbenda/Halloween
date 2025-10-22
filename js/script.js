c92a0a94d2aea52a84911cb90f740fe950225cc5
Sayfbenda <soufiane.bakouri.dev@gmail.com>
2025-10-22 16:13:36 +0200

    Open > toggleSupport

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 var root = document.querySelector(":root")
+const support = document.getElementById("support")
+let supportVisible = false
+
 
 function darkMode() {
     root.style.setProperty("--body-color", "linear-gradient(to right, #111111, #141414, #171717, #191919, #1c1c1c)")
@@ -10,4 +13,14 @@ function normalMode() {
     root.style.setProperty("--body-color", "linear-gradient(90deg, hsl(104, 28%, 40%), hsl(58, 28%, 40%) 100%)")
     root.style.setProperty("--text-color:", "#f5f5f7")
     root.style.setProperty("--container-color", "linear-gradient(136deg, hsl(104, 28%, 35%) 0%, hsl(58, 28%, 35%) 100%)")
+}
+
+function toggleSupport(){
+    if(supportVisible){
+        support.style.visibility = "hidden"
+        supportVisible = false
+    } else {
+        support.style.visibility = "visible"
+        supportVisible = true
+    }
 }
